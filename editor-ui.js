64c809e4678833d14d43ee8e95fd4f01ffba40a0
polo8214 <polo8214@hotmail.com>
2017-06-07 23:02:15 +0800

    補
    補js

---
+++ editor-ui.js
@@ -0,0 +1,165 @@
+function showFileMenu(element, event) {
+    if(document.getElementById("file_menu").style.display == "none")
+		document.getElementById("file_menu").style.display = null;
+    else
+		document.getElementById("file_menu").style.display = "none";
+        element.style.color = "#F39800";
+    event.stopPropagation();
+}
+      function hideMenus() {
+        document.getElementById("file_menu").style.display = "none";
+        document.getElementById("file_menu_root").style.color =  null;
+        Array.from(document.getElementsByClassName("dropdown-content")).forEach(function (x) {
+          x.style.display = "none";
+        })
+      }
+      var webview = document.querySelector('webview')
+      function zooming(scale) {
+        operTp.setScale(scale/100);
+		operTp.zoom();
+		document.getElementById("rangevalue").innerHTML=scale+"%";
+      }
+      interact('.draggable')
+        .draggable({
+            // enable inertial throwing
+            inertia: true,
+            // keep the element within the area of it's parent
+            restrict: {
+                restriction: "parent",
+                endOnly: true,
+                elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+            },
+            // enable autoScroll
+            autoScroll: true,
+            onstart: function (event) {
+            },
+            // call this function on every dragmove event
+            onmove: dragMoveListener,
+            // call this function on every dragend event
+            onend: function (event) {
+                // do the fucking sorting
+                if(event.target.parentElement.id=='tabstrip') {
+                  var tabstrip = document.getElementById('tabstrip')
+                  var p = tabstrip.children[0]
+                  list = []
+                  i = 0
+                  do {
+                    list.push({idx: i, obj: p.cloneNode(true)})
+                    i++
+                  } while(p = p.nextElementSibling)
+
+                  ll = list.sort(function(a, b) {
+                    var ax = (parseFloat(a.obj.getAttribute('data-x')) || 0)+280*a.idx
+                    var bx = (parseFloat(b.obj.getAttribute('data-x')) || 0)+280*b.idx
+                    return ax-bx
+                  })
+                  console.log(ll)
+                  tabstrip.innerHTML = ''
+                  ll.forEach(function (e) {
+                    e.obj.setAttribute('data-x', 0)
+                    e.obj.setAttribute('data-y', 0)
+                    e.obj.style.transform = null
+                    tabstrip.appendChild(e.obj)
+                  })
+                }
+            }
+        });
+
+      interact('.draggable2')
+        .draggable({
+            // enable inertial throwing
+            inertia: false,
+            // keep the element within the area of it's parent
+            restrict: {
+                restriction: "parent",
+                endOnly: true,
+                elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+            },
+            // enable autoScroll
+            autoScroll: true,
+            onstart: function (event) {
+            },
+            // call this function on every dragmove event
+            onmove: dragMoveListener,
+            // call this function on every dragend event
+            onend: function (event) {
+            }
+        });
+    function dragMoveListener (event) {
+        console.log('dragMoveListener');
+        var target = event.target,
+            // keep the dragged position in the data-x/data-y attributes
+            x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+            y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+        // translate the element
+        target.style.webkitTransform =
+            target.style.transform =
+                'translate(' + x + 'px, ' + y + 'px)';
+        // update the position attributes
+        target.setAttribute('data-x', x);
+        target.setAttribute('data-y', y);
+    }
+        function changetab(element, event) {
+          event.stopPropagation()
+          var tabstrip = document.getElementById('tabstrip')
+          var p = tabstrip.children[0]
+          do {
+            if(p == element)
+              p.classList.add('tabediting')
+            else
+              p.classList.remove('tabediting')
+
+          } while(p = p.nextElementSibling)
+          webviews = document.querySelectorAll('webview')
+          webviews.forEach(function(v) {
+            v.classList.add('inactivetab')
+          })
+          webview = document.getElementById(element.id.substring(6))
+          webview.classList.remove("inactivetab")
+          webview.setZoomFactor(document.getElementById('rangeinput').value/100)
+        }
+
+function setwin(flag){
+if (flag=="all") { //以螢幕大小縮放範圍
+resizeTo(screen.availWidth,screen.availHeight);
+moveTo(0,0);
+} else { //預設大小
+resizeTo(930,720);
+moveTo(50,10);
+}
+}
+
+const remote = require('electron').remote;
+
+function Minimize() 
+{
+var window = remote.getCurrentWindow();
+    window.minimize(); 
+}
+
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
