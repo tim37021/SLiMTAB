b9f3ec895fb6d9f3e16c57ba4fc9bfed94c753e1
tim37021 <tim37021@gmail.com>
2017-06-07 22:46:04 +0800

    Made dialog draggable

--- pages/editor-main/script/editor-ui.js
+++ pages/editor-main/script/editor-ui.js
@@ -54,3 +54,37 @@ function showFileMenu(element, event) {
           webview.classList.remove("inactivetab")
           webview.setZoomFactor(document.getElementById('rangeinput').value/100)
         }
+      interact('.dialog')
+        .draggable({
+            // enable inertial throwing
+            inertia: false,
+            // keep the element within the area of it's parent
+            restrict: {
+                restriction: "parent",
+                endOnly: true,
+                elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+            },
+            // enable autoScroll
+            autoScroll: true,
+            onstart: function (event) {
+            },
+            // call this function on every dragmove event
+            onmove: dragMoveListener,
+            // call this function on every dragend event
+            onend: function (event) {
+            }
+        });
+    function dragMoveListener (event) {
+        console.log('dragMoveListener');
+        var target = event.target,
+            // keep the dragged position in the data-x/data-y attributes
+            x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+            y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+        // translate the element
+        target.style.webkitTransform =
+            target.style.transform =
+                'translate(' + x + 'px, ' + y + 'px)';
+        // update the position attributes
+        target.setAttribute('data-x', x);
+        target.setAttribute('data-y', y);
+    }
