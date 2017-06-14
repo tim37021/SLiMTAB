7c07b6b6e4b539f2a1dac0605479fca48a41e66e
tim37021 <tim37021@gmail.com>
2017-06-14 20:19:23 +0800

    Check icon file existance

--- pages/editor-main/ui/buttons/icon.py
+++ pages/editor-main/ui/buttons/icon.py
@@ -6,21 +6,28 @@ def list_icon():
             ret.append(f[7:-4])
     return ret
 
+def file_exist(fname):
+    import os
+    return os.path.isfile(fname) 
+
 def gen_icon_css():
     icons = list_icon()
     for ico in icons:
         print('.'+ico+"icon {")
         print("  background-image: url('./normal/normal_"+ico+".png');")
         print("}")
-        print('.'+ico+"icon-disabled {")
-        print("  background-image: url('./disable/disable_"+ico+".png');")
-        print("}")
-        print('.'+ico+"icon:hover {")
-        print("  background-image: url('./hover/hover_"+ico+".png');")
-        print("}")
-        print('.'+ico+"icon:active {")
-        print("  background-image: url('./pressed/pressed_"+ico+".png');")
-        print("}")
+        if file_exist('./disable/disable_'+ico+'.png'):
+            print('.'+ico+"icon-disabled {")
+            print("  background-image: url('./disable/disable_"+ico+".png');")
+            print("}")
+        if file_exist('./hover/hover_'+ico+'.png'):
+            print('.'+ico+"icon:hover {")
+            print("  background-image: url('./hover/hover_"+ico+".png');")
+            print("}")
+        if file_exist('./pressed/pressed_'+ico+'.png'):
+            print('.'+ico+"icon:active {")
+            print("  background-image: url('./pressed/pressed_"+ico+".png');")
+            print("}")
 
 if __name__ == '__main__':
     gen_icon_css()
